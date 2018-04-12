d52637d1c08c75271d6411fd719b62d930687d90
Unknown <olle@drakenberg.se>
2018-04-12 04:47:39 +0200

    baaajs

---
+++ Depths of Hell NEW_refactorTest/Assets/_RPG core/TargetSystem/MouseManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseManager : MonoBehaviour {
+   public GameObject selectedObject;
+
+   
+    //GameObject selectedObject;
+
+    // Use this for initialization
+    void Start () {
+       
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hitInfo;
+
+        if(Physics.Raycast(ray,out hitInfo))
+        {
+
+            GameObject hitObject = hitInfo.transform.root.gameObject;
+
+            HoveredObject(hitObject);
+
+        }
+        else
+        {
+            ClearSelection();
+        }
+       
+
+	}
+    void HoveredObject(GameObject obj)
+    {
+        if(selectedObject!= null)
+        {
+            if (obj == selectedObject)
+                return;
+
+            ClearSelection();
+        }
+        selectedObject = obj;
+
+        Renderer[] rs = selectedObject.GetComponentsInChildren<Renderer>();
+
+
+        foreach (Renderer r in rs)
+        {
+            Material m = r.material;
+
+            m.color = Color.grey;
+            r.material = m;
+        }
+    }
+
+    void ClearSelection()
+    {
+        if (selectedObject == null)
+            return;
+        Renderer[] rs = selectedObject.GetComponentsInChildren<Renderer>();
+        foreach (Renderer r in rs)
+        {
+            Material m = r.material;
+            m.color = Color.white;
+            r.material = m;
+        }
+        selectedObject = null;
+    }
+}
---
+++ Depths of Hell NEW_refactorTest/Assets/_RPG core/TargetSystem/SelectionIndicator.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectionIndicator : MonoBehaviour {
+
+    MouseManager mm;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+
+        mm = GameObject.FindObjectOfType<MouseManager>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+
+        if(mm.selectedObject != null)
+        {
+
+
+
+
+            Bounds bigBound = mm.selectedObject.GetComponentInChildren<Renderer>().bounds;
+
+            float diameter = bigBound.size.z;
+
+            diameter *= 1.25f;
+
+
+
+            this.transform.position = new Vector3(bigBound.center.x, 0, bigBound.center.z);
+            this.transform.localScale = new Vector3(diameter, 1, diameter);
+
+        }
+            
+	}
+}
